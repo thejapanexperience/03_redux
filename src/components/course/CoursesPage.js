a3f9bce000cec1969c644a43207cf304c842cd2c
Richard Mands <richard@eduku.org>
2017-03-17 20:24:20 +0800

    update

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,10 +1,37 @@
 import React, {PropTypes} from 'react';
 
 class CoursesPage extends React.Component {
-  render () {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { title: null }
+    };
+  }
+
+  onTitleChange(event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({ course: course })
+  }
+
+  onClickSave() {
+    alert(`Saving ${this.state.course.title}`)
+  }
+
+  render() {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+        type="text"
+        onChange={this.onTitleChange}
+        value={this.state.course.title} />
+        <input
+        type="submit"
+        onChange={this.onClickSave}
+        value="Save" />
       </div>
     );
   }
