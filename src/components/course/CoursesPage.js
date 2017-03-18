1ab649a490f240b5855ba93fa8c5c3a336499af6
Richard Mands <richard@eduku.org>
2017-03-18 15:23:30 +0800

    update

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -21,7 +21,7 @@ class CoursesPage extends React.Component {
   }
 
   onClickSave() {
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.createCourse(this.state.course);
   }
 
   courseRow(course, index){
@@ -29,6 +29,7 @@ class CoursesPage extends React.Component {
   }
 
   render() {
+
     return (
       <div>
         <h1>Courses</h1>
@@ -48,8 +49,8 @@ class CoursesPage extends React.Component {
 }
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state, ownProps){
@@ -58,6 +59,10 @@ function mapStateToProps(state, ownProps){
   };
 }
 
-// function mapDispatchToProps()
+function mapDispatchToProps(dispatch){
+  return{
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}
 
-export default connect(mapStateToProps)(CoursesPage);
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
