01885ca551a4bdcded92dbcf1310f830ddca91a7
Richard Mands <richard@eduku.org>
2017-03-18 13:57:17 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,12 +3,18 @@ console.log('shuddappayurfaces');
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
+const store = configureStore(); // can set an initial store here from server or from local storage, etc.
+
 render(
-  <Router history={browserHistory} routes={routes} />,
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
   document.getElementById('app')
 );
