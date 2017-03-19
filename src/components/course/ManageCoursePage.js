0f5c0874e976e57fd7e07b5d09284eb5437f5727
Richard Mands <richard@eduku.org>
2017-03-19 14:07:23 +0800

    update

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -54,8 +54,21 @@ ManageCoursePage.contextTypes = {
 router: PropTypes.object
 };
 
+function getCourseById(courses, id) {
+  const course = courses.filter(course => course.id == id);
+  if (course) return course[0]; // filter returns array so have to grab first item.
+  return null;
+}
+
 function mapStateToProps(state, ownProps) {
+
+  const courseId = ownProps.params.id; // from the path `course/:id`
+
   let course = {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''};
+
+  if (couseId){
+    course = getCourseById(state.courses, courseId)
+  }
 
   const authorsFormattedForDropdown = state.authors.map(author => {
     return {
