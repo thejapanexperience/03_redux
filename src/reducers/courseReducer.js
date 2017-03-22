38df06fa25ccbbed8368776149b68ffa314002f3
Richard Mands <richard@eduku.org>
2017-03-22 16:02:12 +0800

    update

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,5 +1,5 @@
 import * as types from '../actions/actionTypes';
-import initialState from './initialState'
+import initialState from './initialState';
 
 export default function courseReducer(state = initialState.courses, action) {
 
